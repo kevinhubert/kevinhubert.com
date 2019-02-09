a7a905e6931ad270b84fdb0658f4936ea1cc23f6
Kevin Hubert <iamkevinhubert@gmail.come>
2019-02-09 14:44:43 -0600

    adding everything. will remove unused crap later

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,30 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react';
+import Helmet from 'react-helmet';
+import Navigation from './Navigation';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
-import Header from "./header"
-import "./layout.css"
+// Styles
+import '../css/layout.css';
+import '../css/clean-blog.css';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+//Images
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+const Layout = () => (
+  <div>
+    <Helmet>
+      <link
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        rel="stylesheet"
+        type="text/css"
+      />
+    </Helmet>
+    <Navigation />
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
 
-export default Layout
+export default Layout;
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <div>This is the contact page</div>
+    </div>
+  );
+};
